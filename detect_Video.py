07ec732fad8d253544595702f51f120d2ae30aab
nmillorut <nmillorut@gmail.com>
2024-11-10 05:22:41 +0700

    training-1

--- detect_Video.py
+++ detect_Video.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("best2.pt")
 
-results = model.predict(source="test/010.mp4", show = True, save = True, conf=0.4, line_width = 2, save_crop = False, save_txt = False, show_labels = False, classes = [0,1])
+results = model.predict(source="test/010.mp4", show = True, conf=0.4, line_width = 2, save_crop = False, save_txt = False, show_labels = False, classes = [0,1])
