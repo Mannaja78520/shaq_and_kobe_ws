34f9ffdd2f55170be36288d02105ef5b343421df
mannaja <manman7852078520@gmail.com>
2024-11-26 01:14:42 +0700

    update use ros and move image floder

---
+++ robot_ws/src/abu_core/scripts/helloworld.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import rclpy
+import math
+import time
+
+from rclpy import qos
+from rclpy.node import Node
+import rclpy.parameter
+from std_msgs.msg import (
+    String,
+)
+from geometry_msgs.msg import Twist
+import numpy as np
+
+class HelloWorld(Node):
+    def __init__(self):
+        super().__init__("HelloWorld_node")
+        
+        self.hello = self.create_publisher(
+            String,
+            "/helloWorld",
+            qos_profile=qos.qos_profile_sensor_data,
+        )
+        
+        self.sent_timer = self.create_timer(0.1, self.timer_callback)
+        self.i = 0
+        
+        
+    def timer_callback(self):
+        msg_HelloWorld = String()
+        manpub = "Hello am man " + str(self.i)
+        msg_HelloWorld.data = manpub
+        self.hello.publish(msg_HelloWorld)
+        self.i = self.i + 1
+        
+            
+def main():
+    rclpy.init()
+    sub = HelloWorld()
+    rclpy.spin(sub)
+    sub.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
