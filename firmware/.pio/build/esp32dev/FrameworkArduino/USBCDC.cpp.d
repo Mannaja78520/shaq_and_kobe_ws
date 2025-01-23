226aae415a4db67085b1d6d662d80e605abf1cc9
Pha <phakaewpra@gmail.com>
2025-01-23 15:50:06 +0700

    update

--- firmware/.pio/build/esp32dev/FrameworkArduino/USBCDC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/USBCDC.cpp.o: \
- /home/ppha/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.cpp \
- /home/ppha/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /home/ppha/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
