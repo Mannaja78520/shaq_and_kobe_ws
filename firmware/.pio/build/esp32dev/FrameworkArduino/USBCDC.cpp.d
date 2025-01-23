2f283422bb552e7a2a565e4ed0e002802fbe6b05
Pha <phakaewpra@gmail.com>
2025-01-23 15:47:20 +0700

    Push adjust file

---
+++ firmware/.pio/build/esp32dev/FrameworkArduino/USBCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USBCDC.cpp.o: \
+ /home/ppha/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.cpp \
+ /home/ppha/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /home/ppha/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ firmware/.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
+ /home/ppha/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ /home/ppha/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
