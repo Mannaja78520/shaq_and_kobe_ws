d1e763ef99c2c419b91035d61f3d03f77794242f
Pha <phakaewpra@gmail.com>
2025-02-25 03:08:49 +0700

    shaq_fix

--- robot_ws/src/shaq_core/scripts/cmd_vel_to_motor_speed.py
+++ robot_ws/src/shaq_core/scripts/cmd_vel_to_motor_speed.py
@@ -4,176 +4,154 @@
 import threading
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String, Int16MultiArray
 from std_msgs.msg import Float32MultiArray
 from geometry_msgs.msg import Twist
+
 from rclpy import qos
 from src.utilize import * 
 from src.controller import *
 import time 
+import math
+
 
 class Cmd_vel_to_motor_speed(Node):
+
+
+    moveSpeed: float = 0.0
+    slideSpeed: float = 0.0
+    turnSpeed: float = 0.0
+
+    # maxSpeed : int = 1023.0/2 # pwm
+    # max_linear_speed = 2.0  # m/s max
+    motor1Speed : float = 0
+    motor2Speed : float = 0
+    motor3Speed : float = 0
+    motor4Speed : float = 0
+
 
     def __init__(self):
         super().__init__("Cmd_vel_to_motor_speed")
         
-        self.declare_parameter("motor1", True)
-        self.declare_parameter("motor2", True)
-        self.declare_parameter("motor3", True)
-        self.declare_parameter("motor4", True)
+        # self.declare_parameter("motor1", True)
+        # self.declare_parameter("motor2", True)
+        # self.declare_parameter("motor3", True)
+        # self.declare_parameter("motor4", True)
         
-        self.motor1_enabled = self.get_parameter("motor1").get_parameter_value().bool_value
-        self.motor2_enabled = self.get_parameter("motor2").get_parameter_value().bool_value
-        self.motor3_enabled = self.get_parameter("motor3").get_parameter_value().bool_value
-        self.motor4_enabled = self.get_parameter("motor4").get_parameter_value().bool_value
+        # self.motor1_enabled = self.get_parameter("motor1").get_parameter_value().bool_value
+        # self.motor2_enabled = self.get_parameter("motor2").get_parameter_value().bool_value
+        # self.motor3_enabled = self.get_parameter("motor3").get_parameter_value().bool_value
+        # self.motor4_enabled = self.get_parameter("motor4").get_parameter_value().bool_value
 
         
         self.moveSpeed: float = 0.0
+        self.slideSpeed: float = 0.0
         self.turnSpeed: float = 0.0
 
-        self.maxSpeed : float = 1023.0
+        self.maxSpeed : int = 1023.0 # pwm
+        self.max_linear_speed = 3.0  # m/s max
         self.motor1Speed : float = 0
         self.motor2Speed : float = 0
-        
-        self.trackWidth : float = 2.0
-
-        self.yaw : float = 0
-        self.yaw_setpoint = self.yaw
-
-        self.previous_manual_turn = time.time()
-
-        self.controller = Controller(kp = 2.5, ki = 0.05, kd = 0.001, errorTolerance= To_Radians(0.5), i_min= -1, i_max= 1)
-        
-        
+        self.motor3Speed : float = 0
+        self.motor4Speed : float = 0
 
         self.motorshooter1Speed : float = 0
         self.motorshooter2Speed : float = 0
         self.motorshooter3Speed : float = 0
-
-        self.motornadeem : float = 0
-        
+        self.yaw : float = 0
+        self.yaw_setpoint = self.yaw
         
         self.macro_active = False
+        self.previous_manual_turn = time.time()
+
+        self.controller = Controller()
         
  
         
         self.send_robot_speed = self.create_publisher(
-            Twist, "/kobe/cmd_move/rpm", qos_profile=qos.qos_profile_system_default
+            Twist, "/shaq/cmd_move/rpm", qos_profile=qos.qos_profile_system_default
         )
 
         self.send_shoot_speed = self.create_publisher(
-            Twist, "/kobe/cmd_shoot/rpm", qos_profile=qos.qos_profile_system_default
+            Twist, "/shaq/cmd_shoot/rpm", qos_profile=qos.qos_profile_system_default
         )
 
-        self.send_nadeem_speed = self.create_publisher(
-            Twist, "/kobe/cmd_nadeem/rpm", qos_profile=qos.qos_profile_system_default
+
+        self.create_subscription(
+            Twist, '/shaq/cmd_move', self.cmd_vel, qos_profile=qos.qos_profile_system_default
         )
 
         self.create_subscription(
-            Twist, '/kobe/cmd_move', self.cmd_move, qos_profile=qos.qos_profile_system_default
-        )
-
-        self.create_subscription(
-            Twist, '/kobe/cmd_shoot', self.cmd_shoot, qos_profile=qos.qos_profile_sensor_data # 10
+            Twist, '/shaq/cmd_shoot', self.cmd_shoot, qos_profile=qos.qos_profile_sensor_data # 10
         )
         
         self.create_subscription(
-            Twist, '/kobe/cmd_macro', self.cmd_macro, qos_profile=qos.qos_profile_sensor_data # 10
-        )
-        self.create_subscription(
-            Twist, '/kobe/cmd_nadeem', self.cmd_nadeem, qos_profile=qos.qos_profile_sensor_data #10
-        )
-        
-        self.create_subscription(
-              Float32MultiArray, '/kobe/pid/rotate', self.get_pid, qos_profile=qos.qos_profile_sensor_data # 10
-          )
-        
-        self.create_subscription(
-            Twist, '/kobe/imu/pos_angle', self.get_robot_angle, qos_profile=qos.qos_profile_sensor_data # 10
+            Twist, '/shaq/cmd_macro', self.cmd_macro, qos_profile=qos.qos_profile_sensor_data # 10
         )
 
+        self.create_subscription(
+            Twist, '/shaq/imu/pos_angle', self.get_robot_angle, qos_profile=qos.qos_profile_sensor_data # 10
+        )
+
+        self.create_subscription(
+            Float32MultiArray, '/shaq/pid/rotate', self.get_pid, qos_profile=qos.qos_profile_sensor_data # 10
+        )
 
 
         self.sent_data_timer = self.create_timer(0.01, self.sendData)
         
-
     def get_robot_angle(self,msg):
         self.yaw = WrapRads(To_Radians(msg.angular.x))
-
+    
     def get_pid(self,msg):
         self.controller.ConfigPIDF(kp = msg.data[0], ki= msg.data[1], kd=msg.data[2], kf=msg.data[3]) 
 
 
-    def cmd_move(self, msg):
+    def cmd_vel(self, msg):
 
-        # Initialize CurrentTime at the start
         CurrentTime = time.time()
+        self.moveSpeed = msg.linear.y  
+        self.slideSpeed = msg.linear.x  
+        self.turnSpeed = msg.angular.z 
 
-        # Handle turning behavior and set the yaw setpoint
+
         if self.turnSpeed != 0 or (CurrentTime - self.previous_manual_turn < 0.45):
             rotation = self.turnSpeed
             self.yaw_setpoint = self.yaw
-        else:
-            # Apply PID controller based on yaw setpoint
-            rotation = self.controller.Calculate(WrapRads(self.yaw_setpoint - self.yaw))
 
-        # If no movement or turn, stop rotation (ignoring slideSpeed logic)
-        if self.moveSpeed == 0 and self.turnSpeed == 0:
+        rotation = self.controller.Calculate(WrapRads(self.yaw_setpoint - self.yaw)) 
+
+
+        if self.slideSpeed == 0 and self.moveSpeed  == 0 and self.turnSpeed == 0 and abs(rotation) < 0.2:
             rotation = 0
-            self.yaw_setpoint = self.yaw
 
-        # Update the previous manual turn time
         self.previous_manual_turn = CurrentTime if self.turnSpeed != 0 else self.previous_manual_turn
 
-        # Set move and turn speeds from the message input
-        self.moveSpeed = msg.linear.x
-        self.turnSpeed = msg.angular.x 
 
-        # Calculate motor speeds based on move and turn speeds
-        self.motor1Speed = (self.moveSpeed + rotation) * self.maxSpeed / 3
-        self.motor2Speed = (self.moveSpeed - rotation) * self.maxSpeed / 3
-
-        reverse = False  
-
-        # Limit motor speeds to a maximum value
-        max_motor_speed = 1023.0 / 3
-        self.motor1Speed = min(self.motor1Speed, max_motor_speed)
-        self.motor2Speed = min(self.motor2Speed, max_motor_speed)
+        D = max(abs(self.moveSpeed)+abs(self.slideSpeed)+abs(rotation), 1.0)
+        self.motor1Speed = float("{:.1f}".format((self.moveSpeed + self.slideSpeed + rotation) / D * self.maxSpeed))
+        self.motor2Speed = float("{:.1f}".format((self.moveSpeed - self.slideSpeed - rotation) / D * self.maxSpeed))
+        self.motor3Speed = float("{:.1f}".format((self.moveSpeed - self.slideSpeed + rotation) / D * self.maxSpeed))
+        self.motor4Speed = float("{:.1f}".format((self.moveSpeed + self.slideSpeed - rotation) / D * self.maxSpeed))
         
-        # self.motor1Speed = max(min(self.motor1Speed, 1.0), -1.0)
-        # self.motor2Speed = max(min(self.motor2Speed, 1.0), -1.0)
-        
-        # self.motor1Speed = self.map_speed_to_pwm(self.motor1Speed)
-        # self.motor2Speed = self.map_speed_to_pwm(self.motor2Speed)
-        
-    def map_speed_to_pwm(self, speed):
-        # Map motor speed [-1.0, 1.0] to PWM range [1000, 2000]
-        pwm = int(speed * self.maxSpeed)  # Center at 1500ms, range from 1000ms to 2000ms
-        return pwm    
-
 
     def cmd_shoot(self, msg):
-        if not self.macro_active:  # Only update if macro is inactive
-            self.motorshooter1Speed = abs(msg.linear.x - 1) * self.maxSpeed
-            self.motorshooter2Speed = abs(msg.linear.x - 1) * self.maxSpeed
-        
+            if not self.macro_active:  # Only update if macro is inactive
+                self.motorshooter1Speed = abs(msg.linear.x - 1) * self.maxSpeed
+                self.motorshooter2Speed = abs(msg.linear.x - 1) * self.maxSpeed
+            
             self.motorshooter3Speed = abs(msg.linear.z - 1) * self.maxSpeed
             self.motorshooter3Speed += msg.angular.x * self.maxSpeed
 
-        if self.motorshooter3Speed >= 1023.0:
-            self.motorshooter3Speed = 1023.0 
+            if self.motorshooter3Speed >= 1023.0:
+                self.motorshooter3Speed = 1023.0
 
-    def cmd_nadeem(self, msg):
-        if not self.macro_active:  # Only update if macro is inactive
-            self.motornadeem = abs(msg.linear.x - 1) * self.maxSpeed
-            
 
     def cmd_macro(self, msg):
         if msg.linear.x == 1 :
             
             self.macro_active = True
-            self.motorshooter1Speed = 500.0     #Upper
-            self.motorshooter2Speed = 800.0     #Lower
+            self.motorshooter1Speed = -560.0     #Upper
+            self.motorshooter2Speed = 730.0     #Lower
          
         else:
             self.macro_active = False
@@ -182,25 +160,19 @@ class Cmd_vel_to_motor_speed(Node):
     def sendData(self):
         motorspeed_msg = Twist()
         motorshooter_msg = Twist()
-        motornadeem_msg = Twist()
-
        
         motorspeed_msg.linear.x = float(self.motor1Speed)
         motorspeed_msg.linear.y = float(self.motor2Speed)
-
+        motorspeed_msg.angular.x = float(self.motor3Speed)
+        motorspeed_msg.angular.y = float(self.motor4Speed)
 
         motorshooter_msg.linear.x = float(self.motorshooter1Speed)
         motorshooter_msg.linear.y = float(self.motorshooter2Speed)
         motorshooter_msg.linear.z = float(self.motorshooter3Speed)
 
-        motornadeem_msg.linear.x = float(self.motornadeem)
-        motornadeem_msg.angular.x = float(self.yaw)
-
 
         self.send_shoot_speed.publish(motorshooter_msg)
         self.send_robot_speed.publish(motorspeed_msg)
-        self.send_nadeem_speed.publish(motornadeem_msg)
-
 
 
 def main():
