b9adb806cb5be67c12886b271fe51182d82bbc5b
shaq <shaq@shaq.com>
2025-04-03 21:00:49 +0700

    convert bit to rpm

--- robot_ws/src/shaq_core/scripts/cmd_vel_auto_aim.py
+++ robot_ws/src/shaq_core/scripts/cmd_vel_auto_aim.py
@@ -185,12 +185,20 @@ class Cmd_vel_to_motor_speed(Node):
 
         if msg.linear.z == 1:
             self.macro_active = True
-            self.motorshooter1Speed = 730.0  # Upper
-            self.motorshooter2Speed = 770.0  # Lower
+            self.motorshooter1Speed = 4300.0  # Upper
+            self.motorshooter2Speed = 4800.0  # Lower
+
+            # 715 --> 4300
+            # 755 --> 4800
+
         elif msg.linear.x == 1:
             self.macro_active = True
-            self.motorshooter1Speed = -580.0  # Upper
-            self.motorshooter2Speed = 760.0   # Lower
+            self.motorshooter1Speed = -1000.0  # Upper
+            self.motorshooter2Speed = 4800.0   # Lower
+
+            # -580 --> -1000
+            # 760 --> 4800
+
         else:
             self.macro_active = False 
 
