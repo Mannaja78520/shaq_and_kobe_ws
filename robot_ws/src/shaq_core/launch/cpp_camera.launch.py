6d7c49217000bec2fe80f78ce380e27855a0095d
mannaja <manman7852078520@gmail.com>
2025-05-17 22:31:43 +0700

    add monitor camera with ros2

--- robot_ws/src/shaq_core/launch/cpp_camera.launch.py
+++ robot_ws/src/shaq_core/launch/cpp_camera.launch.py
@@ -79,7 +79,7 @@ def generate_launch_description():
     # ld.add_action(cmd_vel_auto_aim)
     ld.add_action(camera_driver)
     # ld.add_action(apriltag_auto_aim)
-    ld.add_action(hoop_detection)
+    # ld.add_action(hoop_detection)
 
 
     return ld
